a8ff685c8bf91cac3edc6768953d2116b7ad566b
Kabir <101722072+KaramatjanovKabir@users.noreply.github.com>
2022-07-23 22:34:51 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-//using selectors inside the element
 const questions = document.querySelectorAll(".question");
 
 questions.forEach(function (question) {
   const btn = question.querySelector(".question-btn");
-  // console.log(btn);
-
   btn.addEventListener("click", function () {
-    // console.log(question);
-
     questions.forEach(function (item) {
       if (item !== question) {
         item.classList.remove("show-text");
@@ -17,14 +12,3 @@ questions.forEach(function (question) {
     question.classList.toggle("show-text");
   });
 });
-
-// traversing the dom
-// const btns = document.querySelectorAll(".question-btn");
-
-// btns.forEach(function (btn) {
-//   btn.addEventListener("click", function (e) {
-//     const question = e.currentTarget.parentElement.parentElement;
-
-//     question.classList.toggle("show-text");
-//   });
-// });
